dbcafacb98d32dbce62267de743b2fdfcec223be
Gokul G S <gokul.codes@gmail.com>
2025-08-25 02:24:43 +0530

    js modification

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ document.querySelectorAll(".drop-down").forEach((toggle) => {
     navLinks.classList.toggle("show");
     hamburger.classList.toggle("open");
   });
+
+
+
+document.querySelectorAll(".drop-down").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const card = btn.closest(".card");
+    const content = card.querySelector(".exp-txt-cont");
+    const img = btn.querySelector("img");
+
+    if (content.classList.contains("open")) {
+     
+      content.style.maxHeight = content.scrollHeight + "px";
+      requestAnimationFrame(() => {
+        content.style.maxHeight = "0px";
+      });
+      content.classList.remove("open");
+      img.classList.remove("rotate");
+    } else {
+      
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.classList.add("open");
+      img.classList.add("rotate");
+
+     
+      content.addEventListener(
+        "transitionend",
+        () => {
+          if (content.classList.contains("open")) {
+            content.style.maxHeight = "none";
+          }
+        },
+        { once: true }
+      );
+    }
+  });
+});
